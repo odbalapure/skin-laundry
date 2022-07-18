c566fc3de20c26d4889711dc92a8626d08113ec0
Om Balapure <odbalapure@gmail.com>
2022-07-18 19:26:12 +0530

    Fix username display

--- pages/success.js
+++ pages/success.js
@@ -22,7 +22,7 @@ function PaymentSuccess() {
             <img height={300} width={400} src="https://cdn.dribbble.com/users/1238709/screenshots/4069900/media/92f9612042676a9db06e67a9533939f0.gif" />
           </div>
           <div className="card-body text-center">
-            <div className="card-text">Hey there <span className="fw-bold">{router.query.name}</span>!</div>
+            <div className="card-text">Hey there <span className="fw-bold">{user}</span>!</div>
             <div className="card-text">Your payment for <span className="fw-bold">{router.query.description}</span> was a success.</div>
           </div>
           <div className="card-footer">
